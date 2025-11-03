116dc6e012c5735b02e57592d5018398b5f57487
Duc <letruyenduc@gmail.com>
2025-11-03 15:30:42 +0100

    Update

--- conf.lua
+++ conf.lua
@@ -5,4 +5,7 @@ function love.conf(t)
     t.console = true
     t.window.height = 1080
     t.window.width = 1920
+    t.window.fullscreen = false
+    t.modules.Joystick = false
+    t.window.resizeable = false
 end
--- main.lua
+++ main.lua
@@ -1,45 +1,70 @@
-_G.love = require("love")
+_G.love = require("love") 
+local game = {
+    difficulty = 1,
+    state = {
+        menu = true,
+        paused = false,
+        running = false,
+        ended = false
+    }
+}
+local player = {
+    x = 40,
+    y = 20,
+    w = 50,
+    h = 50
 
+}
+
+local ennemies = {
+    
+}
 
 function love.load()
+    -- VARIABLES --
     TILE_SIZE = 32
     MAP_WIDTH = 60
     MAP_HEIGHT = 35
     love.window.setMode(MAP_WIDTH * TILE_SIZE, MAP_HEIGHT * TILE_SIZE)
-    SafeX, SafeY, SafeW, SafeH = love.window.getSafeArea()
-    GameMap = {}
-    Player = {}
-    Rectangle = {
-        x = 50,
-        y = 50,
-        w = 50,
-        h = 50
-    }
+    love.mouse.setVisible(false)
+    love.window.setTitle("Project : Rebirth")
+    -- FONCTIONS --
+    function love.playerMovement()
+        if (love.keyboard.isDown("z") and player.y > 0) then
+            player.y = player.y - 10
+        end
+        if (love.keyboard.isDown("d") and player.x >= 0) then
+            player.x = player.x + 10
+        end
+        if (love.keyboard.isDown("s") and player.y >= 0) then
+            player.y = player.y + 10
+        end
+        if (love.keyboard.isDown("q") and player.x > 0) then
+            player.x = player.x - 10
+        end
+    end
+
+    function love.fullscreen()
+        if love.keyboard.isDown("f") then
+            love.window.fullscreen = true
+        end
+    end
+
+    function love.quit()
+        if love.keyboard.isDown("escape") then
+            love.window.close()
+        end
+    end
 end
 
 function love.update(dt)
-    if (love.keyboard.isDown("z") and Rectangle.y > SafeY) then
-        Rectangle.y = Rectangle.y - 1
-    end
-    if (love.keyboard.isDown("d") and Rectangle.x > SafeX) then
-        Rectangle.x = Rectangle.x + 1
-    end
-    if (love.keyboard.isDown("s") and Rectangle.y > SafeY) then
-        Rectangle.y = Rectangle.y + 1
-    end
-    if (love.keyboard.isDown("q") and Rectangle.x > SafeX) then
-        Rectangle.x = Rectangle.x - 1
-    end
-
-    if love.keyboard.isDown("f") then
-        love.window.fullscreen = true
-    end
-    if love.keyboard.isDown("escape") then
-        love.window.close()
-    end
+    love.playerMovement()
+    love.fullscreen()
+    love.quit()
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", Rectangle.x, Rectangle.y, Rectangle.w, Rectangle.h)
+    love.graphics.setColor(0,0.4,0.4)
+    love.graphics.rectangle("fill", player.x, player.y, player.w, player.h)
     love.graphics.printf("FPS:" .. love.timer.getFPS(), love.graphics.newFont(16), 10, 10, love.graphics.getWidth())
 end
