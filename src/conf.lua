121beda3d2d3a10e9561305b3a2d05692190b4b6
Duc <letruyenduc@gmail.com>
2025-11-05 17:36:59 +0100

    Fixation de la fênetre en 1344x840px

---
+++ src/conf.lua
@@ -0,0 +1,12 @@
+function love.conf(t)
+    t.window.title = "Projet jeu"
+    t.identity = "data/saves"
+    t.version = "1.0.0"
+    t.console = true
+    t.window.height = 840
+    t.window.width = 1344 
+    t.modules.Joystick = false
+    t.window.resizable = false
+    t.window.fullscreen = false
+    
+end
